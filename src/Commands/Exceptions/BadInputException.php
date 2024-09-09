8c5df19dfe678d1b4923d9b18f11d74878cc3776
emmanuel campait <ecampait@gmail.com>
2024-09-09 08:46:50 +0200

    Add setup

---
+++ src/Commands/Exceptions/BadInputException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeIgniter\Country\Commands\Exceptions;
+
+use CodeIgniter\Country\Exceptions\RuntimeException;
+
+class BadInputException extends RuntimeException
+{
+
+}
